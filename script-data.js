135c7511069481a5e1e0d8b25d53dd68740a5dfc
sophiedouesneau <sophie.douesneau@gmail.com>
2026-04-12 17:51:54 +0200

    Move conseil block outside purple dropdown in IX

--- script-data.js
+++ script-data.js
@@ -707,10 +707,10 @@ const SCRIPT_DATA = {
               blocks: [
                 { type: "tarifs_tableau" }
               ]
-            },
-            { type: "conseil", text: "👏 En tout cas, ON VOIT :\n- que vous avez essayé de mettre des choses en place\n- et AVEC CE QU'ON vient de voir ensemble :\n  - vous avez toutes les bases\n  - pour que ça évolue dans le bon sens !" }
+            }
           ]
         },
+        { type: "conseil", text: "👏 En tout cas, ON VOIT :\n- que vous avez essayé de mettre des choses en place\n- et AVEC CE QU'ON vient de voir ensemble :\n  - vous avez toutes les bases\n  - pour que ça évolue dans le bon sens !" },
       ]
     },
 
