cb0c43935f9b05903c4d26bb1d27e2fc511ae45f
sophiedouesneau <sophie.douesneau@gmail.com>
2026-05-03 22:26:09 +0200

    a

--- script-data.js
+++ script-data.js
@@ -2902,6 +2902,7 @@ const SCRIPT_DATA = {
             },
             {
               type: "normal", _leftTitle: "Heure repas du soir",
+              _centerContent: true,
               showIfAlim: "Diversification",
               text: "- Pour le **soir**, vous pouvez proposer le **repas… 1h-1h30 avant le coucher**",
               trailingInfoButton: {
